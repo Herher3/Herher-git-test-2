773f4de4e319c2ab32dcc8f55c04bad51dfd1660
Herher3 <fitriherdiantixlrpl2@gmail.com>
2025-07-17 19:59:18 -0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
   <h1>Hallo World!</h1>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae libero culpa impedit omnis fugiat, a facere aperiam esse consequuntur fuga adipisci nam alias temporibus commodi nobis, atque quam sed vel.</p>
+  <ul>
+    <li>satu</li>
+    <li>dua</li>
+  </ul>
 </body>
 </html>
